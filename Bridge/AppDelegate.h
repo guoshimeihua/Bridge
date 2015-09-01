c6acfd0d857240d755023b52d6cce5eeaccf3ef3
Bruce chen <cqdeyouxiang@163.com>
2015-09-01 21:01:14 +0800

    Initial Commit

---
+++ Bridge/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  Bridge
+//
+//  Created by Bruce on 15/9/1.
+//  Copyright (c) 2015年 Bruce. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
